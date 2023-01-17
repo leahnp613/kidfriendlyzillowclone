5e74a03a24839ae549047500299ddefe60db92e9
leahnp613 <leahnagarpowers@gmail.com>
2023-01-17 14:49:10 -0500

    adding headers to realty api service

--- src/services/realtyinus.js
+++ src/services/realtyinus.js
@@ -1,5 +1,9 @@
 const instance = axios.create({
     baseURL: 'realty-in-us.p.rapidapi.com',
     timeout: 1000,
-    headers: {'X-Custom-Header': 'foobar'}
+    headers: {
+        'content-type': 'application/json',
+        'X-RapidAPI-Key': '3f969f773fmsh5981edd5b209872p162883jsn7907b57650ff',
+        'X-RapidAPI-Host': 'realty-in-us.p.rapidapi.com'
+      },
   });

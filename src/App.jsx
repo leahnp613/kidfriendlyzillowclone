a896f2a0e9f2fc84ecc614542a78bac3556cf12d
leahnp613 <leahnagarpowers@gmail.com>
2023-01-18 15:32:26 -0500

    added prettier to format project

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <div>
-        <a href="https://www.google.com/imgres?imgurl=https%3A%2F%2F1000logos.net%2Fwp-content%2Fuploads%2F2017%2F11%2FZillow-Logo.png&imgrefurl=https%3A%2F%2F1000logos.net%2Fzillow-logo%2F&tbnid=-6JlcA0BkDIC4M&vet=12ahUKEwiEj_SC2sL8AhUhs3IEHWk5AesQMygBegUIARC7AQ..i&docid=BdLxlFnYnmUy-M&w=2500&h=750&q=zillow%20logo&ved=2ahUKEwiEj_SC2sL8AhUhs3IEHWk5AesQMygBegUIARC7AQ" target="_blank">
+        <a
+          href="https://www.google.com/imgres?imgurl=https%3A%2F%2F1000logos.net%2Fwp-content%2Fuploads%2F2017%2F11%2FZillow-Logo.png&imgrefurl=https%3A%2F%2F1000logos.net%2Fzillow-logo%2F&tbnid=-6JlcA0BkDIC4M&vet=12ahUKEwiEj_SC2sL8AhUhs3IEHWk5AesQMygBegUIARC7AQ..i&docid=BdLxlFnYnmUy-M&w=2500&h=750&q=zillow%20logo&ved=2ahUKEwiEj_SC2sL8AhUhs3IEHWk5AesQMygBegUIARC7AQ"
+          target="_blank"
+        >
           <img src="/zillow.svg" className="logo" alt="Zillow logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
@@ -28,7 +31,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/services/zillowcom.js
+++ src/services/zillowcom.js
@@ -1,9 +1,13 @@
 const instance = axios.create({
-    baseURL: 'https://zillow-com1.p.rapidapi.com',
-    timeout: 1000,
-    headers: {
-        'content-type': 'application/json',
-        'X-RapidAPI-Key': '3f969f773fmsh5981edd5b209872p162883jsn7907b57650ff',
-        'X-RapidAPI-Host': 'zillow-com1.p.rapidapi.com'
-      },
-  });
+  baseURL: "https://zillow-com1.p.rapidapi.com",
+  timeout: 1000,
+  headers: {
+    "content-type": "application/json",
+    "X-RapidAPI-Key": "3f969f773fmsh5981edd5b209872p162883jsn7907b57650ff",
+    "X-RapidAPI-Host": "zillow-com1.p.rapidapi.com",
+  },
+});
+
+axios.get("/propertyExtendedSearch", {
+  params: { location: "santa monica, ca", home_type: "Houses" },
+});

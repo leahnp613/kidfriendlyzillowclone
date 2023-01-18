39503723eda1707fcb84c2fb1960d692c1cbce8c
leahnp613 <leahnagarpowers@gmail.com>
2023-01-18 15:25:44 -0500

    changed api to zillow clone api, axios instance set up

--- src/services/zillowcom.js
+++ src/services/zillowcom.js
@@ -1,9 +1,9 @@
 const instance = axios.create({
-    baseURL: 'realty-in-us.p.rapidapi.com',
+    baseURL: 'https://zillow-com1.p.rapidapi.com',
     timeout: 1000,
     headers: {
         'content-type': 'application/json',
         'X-RapidAPI-Key': '3f969f773fmsh5981edd5b209872p162883jsn7907b57650ff',
-        'X-RapidAPI-Host': 'realty-in-us.p.rapidapi.com'
+        'X-RapidAPI-Host': 'zillow-com1.p.rapidapi.com'
       },
   });

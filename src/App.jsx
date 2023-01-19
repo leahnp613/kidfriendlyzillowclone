8589cfd96a9132b4f3d6ea26bf9162c90ca25724
leahnp613 <leahnagarpowers@gmail.com>
2023-01-19 11:19:18 -0500

    fixed promise in axios, testing passing basement param

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { useState } from "react";
-import {searchProperty} from './services/zillowcom'
+import { searchProperty } from "./services/zillowcom";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-  searchProperty()
   return (
     <div className="App">
       <div>
@@ -13,7 +12,7 @@ function App() {
           href="https://www.google.com/imgres?imgurl=https%3A%2F%2F1000logos.net%2Fwp-content%2Fuploads%2F2017%2F11%2FZillow-Logo.png&imgrefurl=https%3A%2F%2F1000logos.net%2Fzillow-logo%2F&tbnid=-6JlcA0BkDIC4M&vet=12ahUKEwiEj_SC2sL8AhUhs3IEHWk5AesQMygBegUIARC7AQ..i&docid=BdLxlFnYnmUy-M&w=2500&h=750&q=zillow%20logo&ved=2ahUKEwiEj_SC2sL8AhUhs3IEHWk5AesQMygBegUIARC7AQ"
           target="_blank"
         >
-          <img src="/zillow.svg" className="logo" alt="Zillow logo" />
+          {/* <img src={zillowLogo} className="logo" alt="Zillow logo" /> */}
         </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
--- src/services/zillowcom.js
+++ src/services/zillowcom.js
@@ -4,15 +4,15 @@ const instance = axios.create({
   baseURL: "https://zillow-com1.p.rapidapi.com",
   timeout: 1000,
   headers: {
-    "content-type": "application/json",
     "X-RapidAPI-Key": "3f969f773fmsh5981edd5b209872p162883jsn7907b57650ff",
     "X-RapidAPI-Host": "zillow-com1.p.rapidapi.com",
   },
 });
 
-export const searchProperty = () =>{
+export const searchProperty = (params) => {
+  instance
+    .get("/propertyExtendedSearch", { params })
+    .then((res) => console.log(res.data));
+};
 
-   return axios.get("/propertyExtendedSearch", {
-      params: { location: "santa monica, ca", home_type: "Houses" },
-    }).then( res => console.log(res.data));
-}
+searchProperty({ location:'woodmere, ny',  home_type:'', isBasementFinished: 1});

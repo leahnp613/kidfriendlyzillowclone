ddcac8fe0d2af3f71aab1949c9b2fb3ee9723c93
leahnp613 <leahnagarpowers@gmail.com>
2023-01-23 11:37:04 -0500

    added Formik

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import { searchProperty } from "./services/zillowcom";
 import { SelectionBox } from "./components/SelectionBox";
 import "./App.css";
+import {SearchBox } from './components/SearchBox';
 
 function App() {
   const [count, setCount] = useState(0);
   return (
     <>
       <SelectionBox />
+      <SearchBox />
     </>
   );
 }
